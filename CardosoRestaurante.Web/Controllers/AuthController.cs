4ef71f8d70b2d8f67446301560db3d67a1dc9d7f
johncardosodev <johncardosodev@gmail.com>
2024-05-02 00:38:00 +0100

    Alterei Layout.

---
+++ CardosoRestaurante.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using CardosoRestaurante.Web.Models.DTO;
+using CardosoRestaurante.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CardosoRestaurante.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Registar()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
